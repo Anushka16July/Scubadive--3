8628c85807d233ec6065819a53c7f81b49ff1a37
Anushka16July <87813443+Anushka16July@users.noreply.github.com>
2023-07-04 17:57:12 +0530

    Add files via upload

---
+++ Scuba dive/assets/Game.js
@@ -0,0 +1,10 @@
+const element=document.querySelector(elementId);
+element.addEventListener("collide",e=>{
+    if(elementId.includes("#coin")){
+        element.setAttribute("visible",false);
+        console.log("ring collision");
+    }
+    if(elementId.includes("#fish")){
+        console.log("fish collision");
+    }
+});
